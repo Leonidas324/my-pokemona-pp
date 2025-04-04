d372cc0ce2d5b9b718fd458c07ef41caeb6b6e1e
Hans Siemon <hanssiemon@Hanss-MacBook-Air.local>
2025-04-04 19:52:43 +0200

    j'ai mis certaines choses sur PokemonCard.tsx

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -10,15 +10,32 @@ const pokemonList = [
     },
     {
         name: "mew",
+        imgSrc:  
+        
     },
 ];
 
 function PokemonCard() {
 
- const pokemon= pokemonList[0];
-  pokemon.name = <figcaption>{Pikachu}</figcaption>
-  pokemon.imgSrc = ""
+ const pokemon = pokemonList[1];
+ return (
 
-}
+ <figure>
+  {pokemon.imgSrc ? (
+ <img 
+    src= "https://raw.githubuser.content.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    alt= "bulbasaur"
+    
+    />
+
+  ) : (
+ <p>???</p>
+ 
+ 
+)}
+  <figcaption>{pokemon.name}</figcaption>
+ </figure>
+  );
+};
 
 export default PokemonCard;
