b2cac06921842605274ff57919bc86efde41c3a5
Hans Siemon <hanssiemon@Hanss-MacBook-Air.local>
2025-04-04 12:20:55 +0200

    J'ai mis du contenu sur PokemonCard

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,24 @@
+
+
+
+
+const pokemonList = [
+    {
+  name: "bulbasaur",
+  imgSrc: "https://raw.githubuser.content.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+    },
+    {
+        name: "mew",
+    },
+];
+
+function PokemonCard() {
+
+ const pokemon= pokemonList[0];
+  pokemon.name = <figcaption>{Pikachu}</figcaption>
+  pokemon.imgSrc = ""
+
+}
+
+export default PokemonCard;
