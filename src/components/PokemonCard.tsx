210a3e5ed64c4936de9231510f8946dab6a74a81
Hans Siemon <hanssiemon@Hanss-MacBook-Air.local>
2025-04-05 15:25:38 +0200

    j'ai mis du code sur src/components/PokemonCard.tsx

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -5,7 +5,19 @@
   
   function PokemonCard({pokemon}) {
   
+    return pokemon.imgSrc ? (
+     <figure>
+      <img src={pokemon.imgSrc} alt= {pokemon.name} />
+      <figcaption>{pokemon.name}</figcaption>
+
+     </figure>
+    ) :(
+<figure>
+  <p>???</p>
+  <figcaption>{pokemon.name}</figcaption>
+</figure>
     
+    );
   
   
     
